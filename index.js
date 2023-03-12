0d01c622d5b105629101695f35eb4c7dfcf471bf
John Beltrez <beltrezjohn@gmail.com>
2023-03-12 01:16:29 -0500

    Add index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// class Node() {
+//     this.val = val;
+//     this.left = null; 
+//     this.right = null;
+// }
+// trees are just nested objects
+//  you the user decide how you will traverse the tree traverse means to hit every node once 
+// each node has a left and a right property 
+// in trees the nodes have a relationship to each other 
+// root node does not have a parent 
+// any node that has left and right propeties as node is called a leaf node 
+// a general tree can have as many children as possible but a binary tree can only have 2 children
+// level refers to a group of nodes that are the same distance from the root, therefore, two nodes on the same level dont have to be siblings, moreover level is the number of edges from the root 
+// depth is distance from the root and height is the furthest leaf. *** the height of a tree is the height of the root node 
+// so the reference point for depth is the root 
